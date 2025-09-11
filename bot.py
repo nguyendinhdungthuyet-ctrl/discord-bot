36b5a2855ea971f350834982bc145810a5f86ea4
nguyendinhdungthuyet-ctrl <nguyendinhdungthuyet@gmail.com>
2025-09-11 21:16:20 +0700

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,17 @@
+import discord
+from discord.ext import commands
+import os
+
+intents = discord.Intents.default()
+intents.message_content = True
+bot = commands.Bot(command_prefix="!", intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f"✅ Bot đã đăng nhập thành {bot.user}")
+
+@bot.command()
+async def ping(ctx):
+    await ctx.send("Pong!")
+
+bot.run(os.getenv("DISCORD_TOKEN"))
